f1d9b190f7fb85d5f201f470f7e6af2d39c5f481
icaropinho <icaropinho@ultracold.com.br>
2025-04-12 21:27:11 -0300

    input.jsx

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -4,7 +4,7 @@ export const Input = React.forwardRef(({ className = "", ...props }, ref) => {
   return (
     <input
       ref={ref}
-      className={\`border rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-800 dark:text-white \${className}\`}
+      className={`border rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-800 dark:text-white ${className}`}
       {...props}
     />
   );
